6a0e4cf130594a711ff7347793bfafac778f33c4
Nicole248 <67006553+Nicole248@users.noreply.github.com>
2023-08-29 23:04:13 +0000

    Update MainSection.tsx and NavLogo.tsx

--- src/layout/components/NavLogo.tsx
+++ src/layout/components/NavLogo.tsx
@@ -1,5 +1,15 @@
 import { Box } from "@mui/material";
+import { useRouter } from "next/router";
 
-const NavLogo = () => <Box>NJS</Box>;
+const NavLogo = () => {
+    const router = useRouter();
+    
+    return (
+        <div>
+            {router.pathname === "/" && <Box>Stock Total</Box>}
+            {router.pathname === "/planes" && <Box>Stock Cocina</Box>}
+        </div>
+    )
+} ;
 
 export default NavLogo;
